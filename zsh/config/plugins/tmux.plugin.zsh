7b93b8d68bde08eca09f73b91e0abf7a2241c205
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2023-04-16 20:59:02 +0200

    Nicer tmux shortcut

--- zsh/config/plugins/tmux.plugin.zsh
+++ zsh/config/plugins/tmux.plugin.zsh
@@ -1,1 +1,15 @@
-alias t="tmux"
+# t: act as a shortcut for tmux when invoked with arguments, or attach to a
+# session named after the current directory when invoked without arguments.
+function t() {
+  if [ "$#" -gt 0 ]; then
+    tmux $@
+  else
+    session_name="$(basename $(pwd))"
+    session_exists=$(tmux ls -F '#S' | grep $session_name > /dev/null)
+    if [ $? -eq 0 ]; then
+      tmux attach -t $session_name
+    else
+      tmux new -s $session_name
+    fi
+  fi
+}
