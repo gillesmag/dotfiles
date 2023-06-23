1fd926b7db506d876ca170420b8bc41b5810a251
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2023-06-24 00:36:03 +0200

    Add languages

--- home/kamino/default.nix
+++ home/kamino/default.nix
@@ -50,6 +50,10 @@
       rectangle
       utm
 
+      nodejs_20
+      python3
+      ruby
+
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
       # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
